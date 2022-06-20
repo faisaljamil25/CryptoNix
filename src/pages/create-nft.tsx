e1ac082a5789c720642a37838bbe961a9a481902
Faisal Jamil <faisaljamilcs@gmail.com>
2022-06-21 02:24:06 +0530

    feat: image uploader

--- src/pages/create-nft.tsx
+++ src/pages/create-nft.tsx
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import type { NextPage } from 'next';
+import Image from 'next/image';
+import { useDropzone } from 'react-dropzone';
 
 import { Button, Input } from '../components';
+import images from '../../assets';
+import { useTheme } from 'next-themes';
 
 interface FormInputType {
   price: string;
@@ -10,10 +14,22 @@ interface FormInputType {
 }
 
 const CreateItem: NextPage = () => {
+  const { theme } = useTheme();
   const [formInput, updateFormInput] = useState<FormInputType>({
     price: '',
     name: '',
     description: '',
+  });
+
+  const onDrop = () => {
+    // upload file to the ipfs
+    // to be implemented
+  };
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+    maxSize: 5000000,
   });
 
   return (
@@ -22,6 +38,40 @@ const CreateItem: NextPage = () => {
         <h1 className='font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl'>
           Create new item
         </h1>
+        <div className='mt-16'>
+          <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xl'>
+            Upload file
+          </p>
+          <div className='mt-4'>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <div className='flexCenter flex-col text-center'>
+                <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-xl'>
+                  JPG, PNG, GIF, SVG, WEBM, MP3, MP4. Max 100mb.
+                </p>
+
+                <div className='my-12 w-full flex justify-center'>
+                  <Image
+                    src={images.upload}
+                    width={100}
+                    height={100}
+                    objectFit='contain'
+                    alt='file upload'
+                    className={theme === 'light' ? 'filter invert' : undefined}
+                  />
+                </div>
+
+                <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-sm'>
+                  Drag and Drop File
+                </p>
+                <p className='font-poppins dark:text-white text-nft-black-1 font-semibold text-sm mt-2'>
+                  Or browse media on your device
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <Input
           inputType='input'
           title='Name'
