a8d4e4e4a9a58e266823c4d34853ca98f8bebc2a
Faisal Jamil <faisaljamilcs@gmail.com>
2022-06-22 01:50:23 +0530

    feat: resell token

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -74,7 +74,7 @@ contract NFTMarketplace is ERC721URIStorage {
       );
 
       _transfer(msg.sender, address(this), tokenId);
-      
+
       emit MarketItemCreated(
         tokenId,
         msg.sender,
@@ -82,5 +82,19 @@ contract NFTMarketplace is ERC721URIStorage {
         price,
         false
       );
+    }
+
+    function resellToken(uint256 tokenId, uint256 price) public payable {
+      require(idToMarketItem[tokenId].owner == msg.sender, "Only item owner can perform this operation");
+      require(msg.value == listingPrice, "Price must be equal to listing price");
+
+      idToMarketItem[tokenId].sold = false;
+      idToMarketItem[tokenId].price = price;
+      idToMarketItem[tokenId].seller = payable(msg.sender);
+      idToMarketItem[tokenId].owner = payable(address(this));
+      
+      _itemsSold.decrement();
+
+      _transfer(msg.sender, address(this), tokenId);
     }
 }
