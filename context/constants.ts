dbcb0eac874a326c70beec3e1690db84cbfea45e
Faisal Jamil <faisaljamilcs@gmail.com>
2022-06-23 00:19:51 +0530

    chore: utils

--- context/constants.ts
+++ context/constants.ts
@@ -2,3 +2,23 @@ import marketAbi from './NFTMarketplace.json';
 
 export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 export const MarketAddressABI = marketAbi.abi;
+
+export const nftContextDefaultValues = {
+  nftCurrency: 'ETH',
+  connectWallet: async () => {},
+  currentAccount: '',
+  createSale: async (url: string, formInputPrice: string) => {},
+  fetchNFTs: async () => [
+    {
+      price: '',
+      tokenId: 0,
+      id: 0,
+      seller: '',
+      owner: '',
+      image: '',
+      name: '',
+      description: '',
+      tokenURI: '',
+    },
+  ],
+};
