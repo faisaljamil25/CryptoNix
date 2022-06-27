00ac47d756c2acc5e13accf32a491dff3517f62b
Faisal Jamil <faisaljamilcs@gmail.com>
2022-06-27 21:33:19 +0530

    feat: search for my nfts

--- src/pages/my-nfts.tsx
+++ src/pages/my-nfts.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 
 import { shortenAddress } from '../utils/shortenAddress';
-import { Loader, NFTCard, Banner } from '../components';
+import { Loader, NFTCard, Banner, SearchBar } from '../components';
 import images from '../../assets';
 import { NFTContext } from '../../context/NFTContext';
 import { nftType } from '../../context/types';
@@ -12,13 +12,53 @@ const MyNFTs: NextPage = () => {
   const { fetchMyNFTsOrCreatedNFTs, currentAccount } = useContext(NFTContext);
   const [nfts, setNfts] = useState<nftType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [nftsCopy, setNftsCopy] = useState<nftType[]>([]);
+  const [activeSelect, setActiveSelect] = useState<string>('Recently Added');
 
   useEffect(() => {
     fetchMyNFTsOrCreatedNFTs('fetchMyNFTs').then((items) => {
       setNfts(items);
+      setNftsCopy(items);
       setIsLoading(false);
     });
   }, []);
+
+  useEffect(() => {
+    const sortedNfts = [...nfts];
+
+    switch (activeSelect) {
+      case 'Price (low to high)':
+        setNfts(sortedNfts.sort((a, b) => +a.price - +b.price));
+        break;
+      case 'Price (high to low)':
+        setNfts(sortedNfts.sort((a, b) => +b.price - +a.price));
+        break;
+      case 'Recently added':
+        setNfts(sortedNfts.sort((a, b) => b.tokenId - a.tokenId));
+        break;
+      default:
+        setNfts(nfts);
+        break;
+    }
+  }, [activeSelect]);
+
+  const onHandleSearch = (value: string) => {
+    const filteredNfts = nfts.filter(({ name }) =>
+      name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredNfts.length === 0) {
+      setNfts(nftsCopy);
+    } else {
+      setNfts(filteredNfts);
+    }
+  };
+
+  const onClearSearch = () => {
+    if (nfts.length && nftsCopy.length) {
+      setNfts(nftsCopy);
+    }
+  };
 
   if (isLoading) {
     return (
@@ -59,6 +99,14 @@ const MyNFTs: NextPage = () => {
         </div>
       ) : (
         <div className='sm:px-4 p-12 w-full minmd:w-4/5 flexCenter flex-col'>
+          <div className='flex-1 w-full flex flex-row sm:flex-col px-4 xs:px-0 minlg:px-8'>
+            <SearchBar
+              activeSelect={activeSelect}
+              setActiveSelect={setActiveSelect}
+              handleSearch={onHandleSearch}
+              clearSearch={onClearSearch}
+            />
+          </div>
           <div className='mt-3 w-full flex flex-wrap'>
             {nfts.map((nft) => (
               <NFTCard key={`nft-${nft.tokenId}`} nft={nft} onProfilePage />
